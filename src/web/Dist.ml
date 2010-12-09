7e99a3e8e9d76654a23cfe3449ee3a89ac61a5a5
sylvain.le-gall <sylvain.le-gall@polytechnique.org>
2010-12-09 16:35:18 +0000

    Display a listing of packages/files when browsing dist/
    Ignore-this: 9678e3bd523be8c3c6eb718517235ac
    
    darcs-hash:20101209163518-4d692-5a755dd3e23cae8a714cf1198b47130321ece162

--- src/web/Dist.ml
+++ src/web/Dist.ml
@@ -7,58 +7,203 @@ open Lwt
 open Eliom_services
 open Eliom_parameters
 open Eliom_sessions
-open Eliom_predefmod.Files
+open Eliom_predefmod
+open XHTML.M
 open ODBVer
 open ODBContext
+open ODBGettext
+open Common
+open Template
 open Context
 
 (* TODO: register temporary file *)
 
 let dist = 
-  Eliom_predefmod.Files.register_new_service 
-    ~path:["dist"]
-    ~get_params:(suffix (all_suffix "path"))
+  new_service 
+    ["dist"]
+    (suffix (all_suffix "path"))
+    ()
+
+let mk_ver_fn ~ctxt fn_t ver = 
+  let pkg = 
+    ver.pkg
+  in
+  let ver_str = 
+    OASISVersion.string_of_version
+      ver.ver
+  in
+    ODBStorage.Ver.filename pkg ver_str fn_t
+    >>= fun fn ->
+      let pwd =
+        FileUtil.pwd ()
+      in
+      let fn' =
+        FilePath.make_relative 
+          (FilePath.make_absolute pwd ctxt.odb.dist_dir)
+          (FilePath.make_absolute pwd fn)
+      in
+      let path =
+        (* TODO: create something in FilePath to handle 
+         * this 
+         *)
+        ExtString.String.nsplit fn' "/"
+      in
+      let bn = 
+        FilePath.basename fn'
+      in
+      let classe = 
+        match fn_t with 
+          | `Tarball -> "tarball"
+          | `OASIS | `OASISPristine -> "oasis"
+          | `PluginData _ -> "sexp"
+          | `Other _ -> "other"
+      in
+        if Sys.file_exists fn then
+          return (classe, bn, path)
+        else
+          fail Not_found 
+
+let () = 
+  Any.register 
+    dist
     (fun sp lst () ->
        Context.get ~sp ()
        >>= fun ctxt ->
+       let generate_index lst = 
+         begin
+           match lst with 
+             | [] ->
+                 begin
+                   (* Access to / -> return list of package + top files *)
+                   ODBStorage.Pkg.elements () 
+                   >|= 
+                   (List.map 
+                      (fun pkg ->
+                         "dir",
+                         pkg,
+                         [pkg]))
+                 end
+
+             | [pkg] ->
+                 begin
+                   (* Access to a package  -> list version + package files *)
+                   ODBStorage.Ver.elements pkg
+                   >|= fun lst ->
+                   ("dir", "download", [pkg; "download"])
+                   ::
+                   (List.map 
+                      (fun ver -> 
+                         let ver_str = 
+                           OASISVersion.string_of_version
+                             ver.ver
+                         in
+                           "dir",
+                           ver_str,
+                           [pkg; ver_str])
+                      lst)
+                 end
+
+             | [pkg; "download"] ->
+                 begin
+                   ODBStorage.Ver.elements pkg
+                   >>= 
+                   Lwt_list.map_s (mk_ver_fn ~ctxt `Tarball)
+                 end
+
+             | [pkg; ver_str] ->
+                 (* Access to a package's version file *)
+                 begin
+                   ODBStorage.Ver.find pkg ver_str
+                   >>= fun ver ->
+                   Lwt_list.fold_left_s
+                     (fun acc fn_t ->
+                        catch 
+                          (fun () ->
+                             (mk_ver_fn ~ctxt fn_t ver)
+                             >>= fun e ->
+                             return (e :: acc))
+
+                          (function 
+                             | Not_found ->
+                                 return acc
+                             | e ->
+                                 fail e))
+                     []
+                     [`OASIS; 
+                      `OASISPristine; 
+                      `Tarball]
+                 end
+
+             | _ ->
+                 (* TODO: log *)
+                 fail Eliom_common.Eliom_404
+         end
+
+         >>= fun lst' ->
+         return
+           (List.sort 
+              (fun (_, nm1, _) (_, nm2, _) -> 
+                 OASISVersion.version_compare 
+                   (OASISVersion.version_of_string nm1)
+                   (OASISVersion.version_of_string nm2))
+              lst')
+
+         >>= fun lst' ->
+         Xhtml.send ~sp
+           (template 
+              ~ctxt
+              ~sp
+              ~title:(BrowserAndPageTitle 
+                        (s_ "Dist", 
+                         Printf.sprintf
+                           (f_ "Index of /%s")
+                           (String.concat "/" lst)))
+              ~div_id:"dist"
+              [
+                table
+                  (tr
+                     (th [pcdata (s_ "Name")])
+                     [])
+                  (List.map
+                     (fun (classe, nm, path) ->
+                        tr 
+                          (td 
+                             ~a:[a_class [classe]]
+                             [Xhtml.a dist sp [pcdata nm] path])
+                          [])
+
+                     (match List.rev lst with 
+                        | _ :: tl ->
+                            (* We have an upper dir *)
+                            ("updir", "..", List.rev tl) :: lst'
+                        | [] ->
+                            lst'))
+              ])
+       in
 
        let fn = 
          FilePath.make_filename 
            (ctxt.odb.dist_dir :: lst)
        in
+
          if FilePath.is_subdir ctxt.odb.dist_dir fn then
-           (* TODO: log *)
-           fail
-             Eliom_common.Eliom_404
+           begin
+             fail Eliom_common.Eliom_404
+           end
+
          else
-           return (FilePath.make_absolute (FileUtil.pwd ()) fn))
+           begin
+             let fn_abs = 
+               FilePath.make_absolute (FileUtil.pwd ()) fn
+             in
+               if Sys.file_exists fn_abs && not (Sys.is_directory fn_abs) then
+                 Files.send ~sp fn_abs
+               else
+                 generate_index lst 
+           end)
 
-(* TODO: Is this funtction used ? *)
-
-let a_dist ~sp ~ctxt ver fcontent fn = 
-  (* TODO: avoid too much ODBStorage lookup, by embeding
-   * maximum information in ver (i.e. sexp.blackbox +
-   * add information when loading
-   *)
-  ODBStorage.Ver.filename 
-    ver.pkg 
-    (OASISVersion.string_of_version ver.ver) 
-    fn
-  >>= fun fn ->
-  let fn =
-    let pwd =
-      FileUtil.pwd ()
-    in
-      FilePath.make_relative 
-        (FilePath.make_absolute pwd ctxt.odb.dist_dir)
-        (FilePath.make_absolute pwd fn)
-  in
- 
-  let lst = 
-    (* TODO: create something in FilePath to handle 
-     * this 
-     *)
-    ExtString.String.nsplit fn "/"
-  in
-    return (Eliom_predefmod.Xhtml.a dist sp (fcontent fn) lst, fn)
+let a_dist ~sp ~ctxt ver fcontent fn_t = 
+  mk_ver_fn ~ctxt fn_t ver
+  >|= fun (_, nm, path) ->
+  Xhtml.a dist sp (fcontent nm) path, nm
 
