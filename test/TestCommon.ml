5814a4d9f0ff50c6fc89773bf42142eb10c0585c
sylvain.le-gall <sylvain.le-gall@polytechnique.org>
2011-01-31 10:31:49 +0000

    Speedup ocsigen start, detect appearance of ocsigen command pipe
    Ignore-this: 92e547a4cb7b115cd7d9e7b4f38e304e
    
    darcs-hash:20110131103149-4d692-9d2d908a5a29ef2265b100f60f881c7d90c076c8

--- test/TestCommon.ml
+++ test/TestCommon.ml
@@ -65,242 +65,6 @@ let bracket_tmpdir f =
     (fun dn ->
        FileUtil.rm ~recurse:true [dn])
 
-let bracket_ocsigen conf pre_start f post_stop () =
-
-  let conf_fn, chn = 
-    Filename.open_temp_file "oasis-db-" ".conf"
-  in
-
-  let port =
-    (* Find an available port *)
-    let rec find port = 
-      if is_port_free port then
-        port
-      else
-        find (port + 1)
-    in
-      find ((Random.int 8000) + 1025)
-  in
-
-  let addr = 
-    inet_addr_loopback 
-  in
-
-  let rootdir = 
-    let fn = 
-      Filename.temp_file "oasis-db-" ".dir"
-    in
-      FileUtil.rm [fn];
-      FileUtil.mkdir fn;
-      fn
-  in
-
-  let command_fn = 
-    Filename.concat rootdir "ocsigen_command"
-  in
-
-  let std_conf = 
-    Printf.sprintf 
-      "<port>%s:%d</port>
-       <charset>utf-8</charset>
-       <logdir>%s</logdir>
-       <commandpipe>%s</commandpipe>
-       <extension findlib-package=\"ocsigen.ext.staticmod\"/>
-       <extension findlib-package=\"ocsigen.ext.ocsipersist-sqlite\">
-         <database file=\"%s/ocsidb\"/>
-       </extension>
-       <extension findlib-package=\"ocsigen.ext.eliom\"/>
-       <uploaddir>%s</uploaddir>"
-      (string_of_inet_addr addr)
-      port rootdir command_fn rootdir rootdir
-  in
-
-  let dbug fmt =
-    if !verbose then
-      begin
-        Printf.fprintf Pervasives.stderr "DEBUG: ";
-        Printf.kfprintf 
-          (fun chn -> Printf.fprintf chn "\n%!")
-          Pervasives.stderr
-          fmt
-      end
-    else
-      begin
-        Printf.ifprintf 
-          Pervasives.stderr 
-          fmt
-      end
-  in
-
-  let shutdown pid error_code = 
-    let check_status = 
-      function  
-        | _, Unix.WEXITED code ->
-            OUnit.assert_equal 
-              ~msg:"Error code"
-              error_code
-              code
-
-        | _, _ ->
-            OUnit.assert_failure 
-              (Printf.sprintf 
-                 "Ocsigen process %d exited with error"
-                 pid)
-    in
-
-    (* Still running, ask to exit *)
-    match Unix.waitpid [Unix.WNOHANG] pid with 
-      | 0, _ ->
-          begin
-            if Sys.file_exists command_fn then
-              begin
-                (* Command pipe available *)
-                let chn = open_out command_fn in
-                  dbug "Trying graceful exit for ocsigen process %d" pid;
-                  output_string chn "shutdown\n";
-                  close_out chn
-              end
-            else
-              begin
-                dbug "Killing ocsigen process %d" pid;
-                Unix.handle_unix_error 
-                  (Unix.kill pid) Sys.sigterm
-              end;
-            check_status (Unix.waitpid [] pid)
-          end
-
-      | res ->
-          check_status res
-  in
-
-  let clean () = 
-    FileUtil.rm ~recurse:true [conf_fn; rootdir] 
-  in
-
-  let ocs = 
-    {
-      ocs_port =  port;
-      ocs_addr =  addr;
-      ocs_rootdir = rootdir;
-      ocs_command = command_fn;
-      ocs_base_url = 
-        Printf.sprintf "http://%s:%d/" 
-          (string_of_inet_addr inet_addr_loopback) port;
-    }
-  in
-
-    try 
-      begin
-        let buf = 
-          Buffer.create (String.length conf)
-        in
-        let () = 
-          (* Create configuration file and log dir *)
-          Buffer.add_substitute
-            buf
-            (function
-               | "port" -> (string_of_inet_addr addr)^":"^(string_of_int port)
-               | "rootdir" -> rootdir
-               | "curdir"  -> FileUtil.pwd () 
-               | "std_conf" -> std_conf
-               | "command_fn" -> command_fn
-               | str -> 
-                   invalid_arg 
-                     (Printf.sprintf "with_ocsigen_running(%s)" str))
-            conf;
-          Buffer.output_buffer chn buf;
-          close_out chn;
-          if !verbose then
-            begin
-              Buffer.output_buffer Pervasives.stderr buf;
-              Pervasives.flush Pervasives.stderr
-            end;
-          pre_start ocs
-        in
-
-        (* Start ocsigen *)
-        let pid = 
-          create_process 
-            !ocsigen 
-            (Array.of_list 
-               (!ocsigen 
-                :: 
-                (!ocsigen_args @ ["-c"; conf_fn])))
-            Unix.stdin Unix.stdout Unix.stderr
-        in
-
-        let () = 
-          (* TODO: we should be able to be sure that everything is initialized
-           * here, without sleeping
-           *)
-          Unix.sleep 1
-        in
-
-          (* Test *)
-          begin
-            try 
-              f ocs;
-              (* Stop ocisgen *)
-              shutdown pid 0;
-            with e ->
-              shutdown pid 0;
-              raise e
-          end;
-
-          post_stop ocs;
-          clean ();
-      end
-    with e ->
-      clean ();
-      raise e
-
-let bracket_oasis_db pre_start f post_stop = 
-   bracket_ocsigen
-     "<ocsigen>
-        <server>
-          $std_conf
-          <extension findlib-package=\"pgocaml\" />
-          <extension findlib-package=\"oasis\" />
-          <extension findlib-package=\"sexplib\" />
-          <extension findlib-package=\"inotify\" />
-          <extension findlib-package=\"markdown\" />
-          <extension findlib-package=\"markdown.html\" />
-          <extension findlib-package=\"cameleon.rss\" />
-          <extension findlib-package=\"yojson\" />
-          <extension findlib-package=\"curl\" />
-
-          <host charset=\"utf-8\" >
-            <site path=\"\">
-              <eliom module=\"$curdir/_build/src/rest/rest.cma\" />
-              <eliom module=\"$curdir/_build/src/rest/curl/rest-curl.cma\" />
-              <eliom module=\"$curdir/_build/src/rest/ocsigen/rest-ocsigen.cma\" />
-              <eliom module=\"$curdir/_build/src/lib/oasis-db.cma\" />
-              <eliom module=\"$curdir/_build/src/web/oasis-db-ocsigen.cma\">
-                <dir rel=\"incoming\">$rootdir/incoming</dir>
-                <dir rel=\"dist\">$rootdir/dist</dir>
-              </eliom>
-              <static dir=\"$curdir/src/web/static\" /> 
-            </site>
-          </host>
-        </server>
-      </ocsigen>"
-
-     (fun ocs ->
-        List.iter
-          (fun nm ->
-             FileUtil.mkdir 
-               (Filename.concat ocs.ocs_rootdir nm))
-          ["incoming"; "dist"];
-        pre_start ocs)
-
-     f post_stop
-
-let in_incoming_dir ocs fn = 
-  FilePath.make_filename [ocs.ocs_rootdir; "incoming"; fn]
-
-let in_dist_dir ocs fn =
-  FilePath.make_filename [ocs.ocs_rootdir; "dist"; fn]
-
 module InotifyExt =
 struct 
   let default_timeout = 1.0
@@ -490,3 +254,237 @@ struct
       (Printf.sprintf "File '%s' doesn't match %s" fn what) 
       (wait_change ?timeout fn f)
 end
+
+let bracket_ocsigen conf pre_start f post_stop () =
+
+  let conf_fn, chn = 
+    Filename.open_temp_file "oasis-db-" ".conf"
+  in
+
+  let port =
+    (* Find an available port *)
+    let rec find port = 
+      if is_port_free port then
+        port
+      else
+        find (port + 1)
+    in
+      find ((Random.int 8000) + 1025)
+  in
+
+  let addr = 
+    inet_addr_loopback 
+  in
+
+  let rootdir = 
+    let fn = 
+      Filename.temp_file "oasis-db-" ".dir"
+    in
+      FileUtil.rm [fn];
+      FileUtil.mkdir fn;
+      fn
+  in
+
+  let command_fn = 
+    Filename.concat rootdir "ocsigen_command"
+  in
+
+  let std_conf = 
+    Printf.sprintf 
+      "<port>%s:%d</port>
+       <charset>utf-8</charset>
+       <logdir>%s</logdir>
+       <commandpipe>%s</commandpipe>
+       <extension findlib-package=\"ocsigen.ext.staticmod\"/>
+       <extension findlib-package=\"ocsigen.ext.ocsipersist-sqlite\">
+         <database file=\"%s/ocsidb\"/>
+       </extension>
+       <extension findlib-package=\"ocsigen.ext.eliom\"/>
+       <uploaddir>%s</uploaddir>"
+      (string_of_inet_addr addr)
+      port rootdir command_fn rootdir rootdir
+  in
+
+  let dbug fmt =
+    if !verbose then
+      begin
+        Printf.fprintf Pervasives.stderr "DEBUG: ";
+        Printf.kfprintf 
+          (fun chn -> Printf.fprintf chn "\n%!")
+          Pervasives.stderr
+          fmt
+      end
+    else
+      begin
+        Printf.ifprintf 
+          Pervasives.stderr 
+          fmt
+      end
+  in
+
+  let shutdown pid error_code = 
+    let check_status = 
+      function  
+        | _, Unix.WEXITED code ->
+            OUnit.assert_equal 
+              ~msg:"Error code"
+              error_code
+              code
+
+        | _, _ ->
+            OUnit.assert_failure 
+              (Printf.sprintf 
+                 "Ocsigen process %d exited with error"
+                 pid)
+    in
+
+    (* Still running, ask to exit *)
+    match Unix.waitpid [Unix.WNOHANG] pid with 
+      | 0, _ ->
+          begin
+            if Sys.file_exists command_fn then
+              begin
+                (* Command pipe available *)
+                let chn = open_out command_fn in
+                  dbug "Trying graceful exit for ocsigen process %d" pid;
+                  output_string chn "shutdown\n";
+                  close_out chn
+              end
+            else
+              begin
+                dbug "Killing ocsigen process %d" pid;
+                Unix.handle_unix_error 
+                  (Unix.kill pid) Sys.sigterm
+              end;
+            check_status (Unix.waitpid [] pid)
+          end
+
+      | res ->
+          check_status res
+  in
+
+  let clean () = 
+    FileUtil.rm ~recurse:true [conf_fn; rootdir] 
+  in
+
+  let ocs = 
+    {
+      ocs_port =  port;
+      ocs_addr =  addr;
+      ocs_rootdir = rootdir;
+      ocs_command = command_fn;
+      ocs_base_url = 
+        Printf.sprintf "http://%s:%d/" 
+          (string_of_inet_addr inet_addr_loopback) port;
+    }
+  in
+
+    try 
+      begin
+        let buf = 
+          Buffer.create (String.length conf)
+        in
+        let () = 
+          (* Create configuration file and log dir *)
+          Buffer.add_substitute
+            buf
+            (function
+               | "port" -> (string_of_inet_addr addr)^":"^(string_of_int port)
+               | "rootdir" -> rootdir
+               | "curdir"  -> FileUtil.pwd () 
+               | "std_conf" -> std_conf
+               | "command_fn" -> command_fn
+               | str -> 
+                   invalid_arg 
+                     (Printf.sprintf "with_ocsigen_running(%s)" str))
+            conf;
+          Buffer.output_buffer chn buf;
+          close_out chn;
+          if !verbose then
+            begin
+              Buffer.output_buffer Pervasives.stderr buf;
+              Pervasives.flush Pervasives.stderr
+            end;
+          pre_start ocs
+        in
+
+        (* Start ocsigen *)
+        let pid = 
+          create_process 
+            !ocsigen 
+            (Array.of_list 
+               (!ocsigen 
+                :: 
+                (!ocsigen_args @ ["-c"; conf_fn])))
+            Unix.stdin Unix.stdout Unix.stderr
+        in
+
+        let () = 
+          InotifyExt.assert_create_file command_fn
+        in
+
+          (* Test *)
+          begin
+            try 
+              f ocs;
+              (* Stop ocisgen *)
+              shutdown pid 0;
+            with e ->
+              shutdown pid 0;
+              raise e
+          end;
+
+          post_stop ocs;
+          clean ();
+      end
+    with e ->
+      clean ();
+      raise e
+
+let bracket_oasis_db pre_start f post_stop = 
+   bracket_ocsigen
+     "<ocsigen>
+        <server>
+          $std_conf
+          <extension findlib-package=\"pgocaml\" />
+          <extension findlib-package=\"oasis\" />
+          <extension findlib-package=\"sexplib\" />
+          <extension findlib-package=\"inotify\" />
+          <extension findlib-package=\"markdown\" />
+          <extension findlib-package=\"markdown.html\" />
+          <extension findlib-package=\"cameleon.rss\" />
+          <extension findlib-package=\"yojson\" />
+          <extension findlib-package=\"curl\" />
+
+          <host charset=\"utf-8\" >
+            <site path=\"\">
+              <eliom module=\"$curdir/_build/src/rest/rest.cma\" />
+              <eliom module=\"$curdir/_build/src/rest/curl/rest-curl.cma\" />
+              <eliom module=\"$curdir/_build/src/rest/ocsigen/rest-ocsigen.cma\" />
+              <eliom module=\"$curdir/_build/src/lib/oasis-db.cma\" />
+              <eliom module=\"$curdir/_build/src/web/oasis-db-ocsigen.cma\">
+                <dir rel=\"incoming\">$rootdir/incoming</dir>
+                <dir rel=\"dist\">$rootdir/dist</dir>
+              </eliom>
+              <static dir=\"$curdir/src/web/static\" /> 
+            </site>
+          </host>
+        </server>
+      </ocsigen>"
+
+     (fun ocs ->
+        List.iter
+          (fun nm ->
+             FileUtil.mkdir 
+               (Filename.concat ocs.ocs_rootdir nm))
+          ["incoming"; "dist"];
+        pre_start ocs)
+
+     f post_stop
+
+let in_incoming_dir ocs fn = 
+  FilePath.make_filename [ocs.ocs_rootdir; "incoming"; fn]
+
+let in_dist_dir ocs fn =
+  FilePath.make_filename [ocs.ocs_rootdir; "dist"; fn]
+
